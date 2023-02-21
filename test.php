f132cfc7889e39ef4c657ef224959a266645539f
Matin <goshvek@gmail.com>
2023-02-21 23:03:31 +0330

    add test.php to project

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+
+    $s = new SplObjectStorage();
+    $o1 = new stdClass;
+    $s[$o1] = "ali";
+    var_dump($s);
+
+    $s = new WeakMap();
+    $o1 = new stdClass;
+    $s[$o1] = "ali";
+    var_dump($s);
+
+    $std = new stdClass;
+    var_dump(serialize($std));
+    $std = serialize($std);
+    var_dump(unserialize($std));
+
+    var_dump("OK");
+
+?>
